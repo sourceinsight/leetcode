2afe88740556c6332fff6e380ae245b9e3a9957d
Michael <bai_he@126.com>
2015-04-04 22:47:47 +0800

    add solution of Number of 1 Bits

---
+++ Number_of_1_Bits.cc
@@ -0,0 +1,21 @@
+/*
+Write a function that takes an unsigned integer and returns the number of ’1' bits it has (also known as the Hamming weight).
+
+For example, the 32-bit integer ’11' has binary representation 00000000000000000000000000001011, so the function should return 3.
+*/
+
+class Solution 
+{
+public:
+    int hammingWeight(uint32_t n) 
+    {
+        int cnt = 0;
+        for (int i = 0; i < sizeof(n)*8; i++)
+        {
+            if ((n >> i) & 0x01)
+                cnt++;
+        }
+        
+        return cnt;
+    }
+};
