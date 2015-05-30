1c65c05f2efc542c15506af963d48947bee7c941
Michael <bai_he@126.com>
2015-05-30 16:53:59 +0800

    add solution of Contains Duplicate

---
+++ Contains_Duplicate.cc
@@ -0,0 +1,29 @@
+/*
+Given an array of integers, find if the array contains any duplicates. 
+Your function should return true if any value appears at least twice in the array, 
+and it should return false if every element is distinct.
+*/
+
+class Solution 
+{
+public:
+    bool containsDuplicate(vector<int>& nums) 
+    {
+        if (nums.size() <= 0)
+            return false;
+            
+        unordered_map<int, int> mapping;
+        
+        for (int i = 0; i < nums.size(); i++)
+        {
+            mapping[nums[i]] = i; 
+        }
+        
+        if (mapping.size() < nums.size())
+        {
+            return true;
+        }
+        
+        return false;
+    }
+};
